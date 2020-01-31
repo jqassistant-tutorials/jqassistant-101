e6547d9ac8bb1c32fd088568afcb3e13149ad54d
Dirk Mahler <dirk.mahler@buschmais.com>
2020-01-31 22:29:24 +0100

    updated sharing rules tutorial for jQA 1.8.0

--- src/main/tutorials/sharing-rules-between-projects/rules/src/test/java/my/organization/rules/PackageTest.java
+++ src/main/tutorials/sharing-rules-between-projects/rules/src/test/java/my/organization/rules/PackageTest.java
@@ -1,23 +1,21 @@
 package my.organization.rules;
 
-import static com.buschmais.jqassistant.core.analysis.api.Result.Status.FAILURE;
-import static com.buschmais.jqassistant.core.analysis.api.Result.Status.SUCCESS;
-import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import com.buschmais.jqassistant.core.report.api.model.Result;
+import com.buschmais.jqassistant.core.rule.api.model.Constraint;
+import com.buschmais.jqassistant.core.rule.api.model.RuleException;
+import com.buschmais.jqassistant.plugin.java.api.model.TypeDescriptor;
+import com.buschmais.jqassistant.plugin.java.test.AbstractJavaPluginIT;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.buschmais.jqassistant.core.analysis.api.Result;
-import com.buschmais.jqassistant.core.analysis.api.rule.Constraint;
-import com.buschmais.jqassistant.core.analysis.api.rule.RuleException;
-import com.buschmais.jqassistant.plugin.java.api.model.TypeDescriptor;
-import com.buschmais.jqassistant.plugin.java.test.AbstractJavaPluginIT;
-
-import org.junit.Test;
+import static com.buschmais.jqassistant.core.report.api.model.Result.Status.FAILURE;
+import static com.buschmais.jqassistant.core.report.api.model.Result.Status.SUCCESS;
+import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
+import static org.assertj.core.api.Assertions.assertThat;
 
 // tag::Test[]
 public class PackageTest extends AbstractJavaPluginIT {
@@ -27,12 +25,12 @@ public class PackageTest extends AbstractJavaPluginIT {
      * are located within the given root package.
      */
     @Test
-    public void validRootPackage() throws IOException, RuleException {
+    public void validRootPackage() throws RuleException {
         scanClassPathDirectory(getClassesDirectory(PackageTest.class));
         HashMap<String, String> parameters = new HashMap<>();
         parameters.put("rootPackage", PackageTest.class.getPackage().getName());
 
-        assertThat(validateConstraint("package:RootPackage", parameters).getStatus(), equalTo(SUCCESS));
+        assertThat(validateConstraint("package:RootPackage", parameters).getStatus()).isEqualTo(SUCCESS);
     }
 
     /**
@@ -40,7 +38,7 @@ public class PackageTest extends AbstractJavaPluginIT {
      * located outside the given root package.
      */
     @Test
-    public void invalidRootPackage() throws IOException, RuleException {
+    public void invalidRootPackage() throws RuleException {
         scanClassPathDirectory(getClassesDirectory(PackageTest.class));
         HashMap<String, String> parameters = new HashMap<>();
         parameters.put("rootPackage", "org.jqassistant");
@@ -48,12 +46,12 @@ public class PackageTest extends AbstractJavaPluginIT {
         Result<Constraint> result = validateConstraint("package:RootPackage", parameters);
 
         store.beginTransaction();
-        assertThat(result.getStatus(), equalTo(FAILURE));
+        assertThat(result.getStatus()).isEqualTo(FAILURE);
         List<Map<String, Object>> rows = result.getRows();
-        assertThat(rows.size(), equalTo(1));
+        assertThat(rows.size()).isEqualTo(1);
         Map<String, Object> row = rows.get(0);
         TypeDescriptor typeOutsideRootPackage = (TypeDescriptor) row.get("TypeOutsideRootPackage");
-        assertThat(typeOutsideRootPackage, typeDescriptor(PackageTest.class));
+        MatcherAssert.assertThat(typeOutsideRootPackage, typeDescriptor(PackageTest.class));
         store.commitTransaction();
     }
 }
